7171d3bc83c531492b73f5e8aca77fbe7a626b92
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2017-02-24 08:55:54 +0200

    Ebin :D

--- core/src/com/mygdx/game/ChaosCompany.java
+++ core/src/com/mygdx/game/ChaosCompany.java
@@ -1,13 +1,13 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class ChaosCompany extends ApplicationAdapter {
+public class ChaosCompany extends Game {
 	SpriteBatch batch;
-	//Sami on ihan gei
 	
 	@Override
 	public void create () {
@@ -16,10 +16,7 @@ public class ChaosCompany extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.end();
+        super.render();
 	}
 	
 	@Override
