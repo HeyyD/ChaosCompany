a13bab7aa42454d6be326ca00b7309ba07ef5b8a
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2017-02-26 17:34:44 +0200

    Kameraa lisätty menuun että näyttää järkevältä androidilla.

--- core/src/com/mygdx/game/MainMenuState.java
+++ core/src/com/mygdx/game/MainMenuState.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -13,6 +14,7 @@ import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /**
  * Created by SamiH on 24.2.2017.
@@ -24,9 +26,10 @@ public class MainMenuState implements Screen {
     private Texture img;
     private Stage stage;
     private SpriteBatch batch;
+    private OrthographicCamera camera;
 
-    private final int SCREEN_WIDTH = 800;
-    private final int SCREEN_HEIGHT = 480;
+    private final float SCREEN_WIDTH = 800;
+    private final float SCREEN_HEIGHT = 480;
 
     public MainMenuState(ChaosCompany g){
         this.game = g;
@@ -35,13 +38,14 @@ public class MainMenuState implements Screen {
 
     public void create(){
         batch =         new SpriteBatch();
-        stage =         new Stage();
+        stage =         new Stage(new FitViewport(SCREEN_WIDTH,SCREEN_HEIGHT));
+
         Gdx.input.setInputProcessor(stage);
 
         //Setting up skin color and size
         skin = new Skin();
         Pixmap pixmap = new Pixmap(200,60, Pixmap.Format.RGBA8888);
-        pixmap.setColor(Color.GREEN);
+        pixmap.setColor(Color.RED);
         pixmap.fill();
 
         skin.add("white", new Texture(pixmap));
@@ -70,13 +74,11 @@ public class MainMenuState implements Screen {
                 System.out.println("X: "+ x + "Y: " + y);
                 return true;
             }
-
             public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
                 //if user is not on top of the button anymore, it dosent do anything
                 if(x > 0 && x < 200 && y > 0 && y < 60){
                     game.setScreen(game.getOfficeState());
                 }
-
             }
         });
 
@@ -92,13 +94,16 @@ public class MainMenuState implements Screen {
 
     @Override
     public void render(float delta) {
+
         //Clear screen
         Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         //Draw everything
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        batch.begin();
         stage.draw();
+        batch.end();
     }
 
     @Override
@@ -123,7 +128,8 @@ public class MainMenuState implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
+        skin.dispose();
     }
 
     public ChaosCompany getGame(){
