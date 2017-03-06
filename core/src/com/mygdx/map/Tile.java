a3a86c7c87bb965107c9835f7c5d66e3ab20b70c
HeyyD <heidi.hati@cs.tamk.fi>
2017-03-06 15:24:55 +0200

    Alustava Tile luokka

--- core/src/com/mygdx/map/Tile.java
+++ core/src/com/mygdx/map/Tile.java
@@ -1,6 +1,19 @@
 package com.mygdx.map;
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 public class Tile {
 
+    private boolean isFull = false;
+    private TextureRegion texture;
+
+    public Tile(TextureRegion texture){
+        this.texture = texture;
+    }
+
+    public void draw(SpriteBatch batch, float x, float y, float width, float height){
+        batch.draw(texture, x, y, width, height);
+    }
 
 }
--- core/src/com/mygdx/map/TileMap.java
+++ core/src/com/mygdx/map/TileMap.java
@@ -13,24 +13,36 @@ public class TileMap {
     private SpriteBatch         spriteBatch = null;
     private int[][]             map = null;
     private Texture             textureTileset = null;
+    private final int           TILE_WIDTH_PIXELS = 64;
+    private final int           TILE_HEIGHT_PIXELS = 32;
     private TextureRegion[]		tileSet = null;
     private int                 tileSetWidth = 4;
+    private Tile[][]            tiles = null;
 
     public TileMap (int[][] map, SpriteBatch spriteBatch){
         this.map = map;
+        tiles = new Tile[map.length][map[0].length];
+
         this.spriteBatch = spriteBatch;
 
         textureTileset = new Texture("tileset.png");
         textureTileset.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
         tileSet = new TextureRegion[tileSetWidth];
         for(int x = 0; x < tileSet.length; x++){
-            tileSet[x] = new TextureRegion(textureTileset, x*64, 0, 64, 32);
+            tileSet[x] = new TextureRegion(textureTileset, x * TILE_WIDTH_PIXELS, 0, TILE_WIDTH_PIXELS, TILE_HEIGHT_PIXELS);
+        }
+
+        //Create tiles according to the map
+        for(int i = 0; i < tiles.length; i++){
+            for(int j = 0; j < tiles[i].length; j++){
+                tiles[i][j] = new Tile(tileSet[map[i][j]]);
+            }
         }
     }
 
     public void renderMap(){
-        for (int x = 0; x < map.length; x++){
-            for(int y = map[x].length - 1; y >= 0; y--){
+        for (int x = 0; x < tiles.length; x++){
+            for(int y = tiles[x].length - 1; y >= 0; y--){
 
                 float x_pos = (x * tileWidth /2.0f ) + (y * tileWidth / 2.0f);
                 float y_pos = - (x * tileHeight / 2.0f) + (y * tileHeight /2.0f);
@@ -40,7 +52,7 @@ public class TileMap {
                 else
                     spriteBatch.setColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-                spriteBatch.draw(tileSet[map[x][y]], x_pos, y_pos, tileWidth, tileHeight);
+                tiles[x][y].draw(spriteBatch, x_pos, y_pos, tileWidth, tileHeight);
             }
         }
     }
