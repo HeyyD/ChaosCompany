995ae18f723e36fcd0c14625c5135d918ffe37d9
HeyyD <heidi.hati@cs.tamk.fi>
2017-04-20 17:38:26 +0300

    Uus main menu

--- core/src/com/mygdx/game/MainMenuState.java
+++ core/src/com/mygdx/game/MainMenuState.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
@@ -23,17 +24,23 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 public class MainMenuState implements Screen {
     private Skin skin;
     private ChaosCompany game;
-    private Texture backgroundImage;
     private Stage stage;
     private SpriteBatch batch;
     private OrthographicCamera camera;
     private I18NBundle bundle = ChaosCompany.myBundle;
 
+    //graphics
+    private Texture backgroundImage;
+    private Sprite earth;
+    private Sprite earthRim;
+    private Texture title;
 
     private final float SCREEN_WIDTH = 800;
     private final float SCREEN_HEIGHT = 480;
     private final int BUTTON_WIDTH = 200;
-    private final int BUTTON_HEIGHT = 60;
+    private final int BUTTON_HEIGHT = 50;
+    private int offset = 10;
+    private float rotationSpeed = 0.10f;
 
     public MainMenuState(ChaosCompany g){
         this.game = g;
@@ -44,41 +51,31 @@ public class MainMenuState implements Screen {
         batch =             new SpriteBatch();
         stage =             new Stage(new FitViewport(SCREEN_WIDTH,SCREEN_HEIGHT));
         camera =            new OrthographicCamera();
-        backgroundImage =   new Texture("menubackground.jpeg");
+        backgroundImage =   new Texture("TitleScreen/background.png");
+        earth =             new Sprite(new Texture("TitleScreen/earth.png"));
+        earthRim =          new Sprite(new Texture("TitleScreen/earthRim.png"));
+        title =             new Texture("TitleScreen/title.png");
+
+        earthRim.setOriginCenter();
+        earthRim.setPosition(100, -SCREEN_HEIGHT/1.5f);
+        earth.setOriginCenter();
+        earth.setPosition(earthRim.getX() + 100, earthRim.getY() + 100);
 
         camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
         batch.setProjectionMatrix(camera.combined);
 
         //Setting up skin color and size of button
-        skin = new Skin();
-        Pixmap pixmap = new Pixmap(BUTTON_WIDTH,BUTTON_HEIGHT, Pixmap.Format.RGBA8888);
-        pixmap.setColor(Color.RED);
-        pixmap.fill();
-
-        skin.add("white", new Texture(pixmap));
-
-        //Setting up skin font of button.
-        BitmapFont bfont = new BitmapFont();
-        skin.add("default", bfont);
-
-        //Config TextButtonStyle and name it "default"
-        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
-        textButtonStyle.down = skin.newDrawable("white", Color.LIGHT_GRAY);
-        textButtonStyle.over = skin.newDrawable("white", Color.DARK_GRAY);
-
-        textButtonStyle.font = skin.getFont("default");
-
-        skin.add("default", textButtonStyle);
+        skin = new Skin(Gdx.files.internal("flat-earth-ui.json"));
 
         //Create TextButton three TextButtons
-        final TextButton playBtn = new TextButton(bundle.get("play"),textButtonStyle);
-        final TextButton settingsBtn = new TextButton("SETTINGS", textButtonStyle);
-        final TextButton exitBtn = new TextButton(bundle.get("exit"), textButtonStyle);
+        final TextButton playBtn = new TextButton(bundle.get("play"),skin);
+        final TextButton exitBtn = new TextButton(bundle.get("exit"), skin);
 
+        playBtn.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        exitBtn.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
 
         //PLAYBUTTON
-        playBtn.setPosition(SCREEN_WIDTH/2 - BUTTON_WIDTH/2 , SCREEN_HEIGHT/2 - BUTTON_HEIGHT /2);
+        playBtn.setPosition(SCREEN_WIDTH/2 - BUTTON_WIDTH/2 , SCREEN_HEIGHT/4 - BUTTON_HEIGHT /2);
         //Add InputListener to playBtn
         playBtn.addListener(new InputListener() {
             public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
@@ -94,27 +91,8 @@ public class MainMenuState implements Screen {
         });
         stage.addActor(playBtn);
 
-        //SETTINGS BUTTON
-        settingsBtn.setPosition(SCREEN_WIDTH/2 - BUTTON_WIDTH/2 , SCREEN_HEIGHT/2 - BUTTON_HEIGHT /2
-                - BUTTON_HEIGHT - 10);
-        //Add InputListener to settingsBtn
-        settingsBtn.addListener(new InputListener() {
-            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                //System.out.println("X: "+ x + "Y: " + y);
-                return true;
-            }
-            public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-                //if user is not on top of the button anymore, it dosent do anything
-                if(x > 0 && x < BUTTON_WIDTH && y > 0 && y < BUTTON_HEIGHT){
-                    game.setScreen(game.getOfficeState());
-                }
-            }
-        });
-        stage.addActor(settingsBtn);
-
         //EXIT BUTTON
-        exitBtn.setPosition(SCREEN_WIDTH/2 - BUTTON_WIDTH/2 , SCREEN_HEIGHT/2 - BUTTON_HEIGHT /2
-                - BUTTON_HEIGHT * 2 - 40);
+        exitBtn.setPosition(playBtn.getX(), playBtn.getY() - (playBtn.getHeight() + offset));
         //Add InputListener to exitBtn
         exitBtn.addListener(new InputListener() {
             public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
@@ -146,7 +124,12 @@ public class MainMenuState implements Screen {
         //Draw everything
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         batch.begin();
-        batch.draw(backgroundImage,0,0);
+        batch.draw(backgroundImage, 0, 0, camera.viewportWidth, camera.viewportHeight);
+        earthRim.draw(batch);
+        earthRim.rotate(rotationSpeed);
+        earth.draw(batch);
+        earth.rotate(-rotationSpeed/2);
+        batch.draw(title, 80, SCREEN_HEIGHT/2);
         batch.end();
         stage.draw();
     }
